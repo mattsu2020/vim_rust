fbf9c59bcc058fcbeb6236e5ceeeaaa21931533e
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-09-01 23:28:43 +0900

    Expand Rust JSON API and delegate C stubs

--- src/json.c
+++ src/json.c
@@ -2,16 +2,27 @@
 
 #if defined(FEAT_EVAL) || defined(PROTO)
 
-// Declarations for Rust FFI functions.
-extern char_u *json_encode_rs(const char *input);
-extern char_u *json_decode_rs(const char *input);
+// Structures and declarations for Rust FFI functions.
+typedef struct {
+    char_u *ptr;
+    size_t used;
+    int error;
+} JsonDecodeResult;
+
+typedef struct {
+    size_t used;
+    int status;
+} JsonFindEndResult;
+
+extern char_u *json_encode_rs(const char *input, int options);
+extern JsonDecodeResult json_decode_rs(const char *input, int options);
+extern JsonFindEndResult json_find_end_rs(const char *input, int options);
 
 // Basic JSON encode wrapper using Rust implementation.
 char_u *
 json_encode(typval_T *val, int options)
 {
-    // Ignoring options for the simplified Rust implementation.
-    return json_encode_rs((const char *)tv_get_string(val));
+    return json_encode_rs((const char *)tv_get_string(val), options);
 }
 
 // Encode [nr, val] expression; simplified to encode the value only.
@@ -34,19 +45,24 @@ json_encode_lsp_msg(typval_T *val)
 int
 json_decode(js_read_T *reader, typval_T *res, int options)
 {
-    (void)options;
+    JsonDecodeResult r = json_decode_rs((const char *)reader->js_buf, options);
+    reader->js_used = (int)r.used;
     res->v_type = VAR_STRING;
-    res->vval.v_string = json_decode_rs((const char *)reader->js_buf);
-    return OK;
+    res->vval.v_string = r.ptr;
+    if (r.error == 0)
+        return OK;
+    return r.error == 1 ? MAYBE : FAIL;
 }
 
-// Find the end of a JSON message; this stub always succeeds.
+// Find the end of a JSON message using the Rust implementation.
 int
 json_find_end(js_read_T *reader, int options)
 {
-    (void)reader;
-    (void)options;
-    return OK;
+    JsonFindEndResult r = json_find_end_rs((const char *)reader->js_buf, options);
+    reader->js_used = (int)r.used;
+    if (r.status == 1)
+        return OK;
+    return r.status == 2 ? MAYBE : FAIL;
 }
 
 // Vim script interfaces -----------------------------------------------------
@@ -54,22 +70,24 @@ json_find_end(js_read_T *reader, int options)
 void
 f_js_decode(typval_T *argvars, typval_T *rettv)
 {
+    JsonDecodeResult r = json_decode_rs((const char *)tv_get_string(&argvars[0]), 0);
     rettv->v_type = VAR_STRING;
-    rettv->vval.v_string = json_decode_rs((const char *)tv_get_string(&argvars[0]));
+    rettv->vval.v_string = r.ptr;
 }
 
 void
 f_js_encode(typval_T *argvars, typval_T *rettv)
 {
     rettv->v_type = VAR_STRING;
-    rettv->vval.v_string = json_encode_rs((const char *)tv_get_string(&argvars[0]));
+    rettv->vval.v_string = json_encode_rs((const char *)tv_get_string(&argvars[0]), 0);
 }
 
 void
 f_json_decode(typval_T *argvars, typval_T *rettv)
 {
+    JsonDecodeResult r = json_decode_rs((const char *)tv_get_string(&argvars[0]), 0);
     rettv->v_type = VAR_STRING;
-    rettv->vval.v_string = json_decode_rs((const char *)tv_get_string(&argvars[0]));
+    rettv->vval.v_string = r.ptr;
 }
 
 void
