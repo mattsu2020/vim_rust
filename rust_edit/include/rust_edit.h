b9ccb37c88e9bf3cf9d87872cebdaaac68e667fe
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-09-02 09:31:46 +0900

    Add rust_edit crate for backspace and completion

---
+++ rust_edit/include/rust_edit.h
@@ -0,0 +1,11 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+extern const uint8_t BACKSPACE;
+
+/**
+ * Equivalent of Vim's CTRL macro. Shared with C via cbindgen.
+ */
+uint8_t ctrl(uint8_t c);
