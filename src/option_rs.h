28b66dac6c395c9e15947e3aa220082076444f0f
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-09-02 13:17:11 +0900

    Add Rust-backed option parser

---
+++ src/option_rs.h
@@ -0,0 +1,16 @@
+#ifndef OPTION_RS_H
+#define OPTION_RS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "rust_option.h"
+
+bool rs_parse_option(const char *assignment);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // OPTION_RS_H
