727b8a16447bec714512a3167a9c9f8a18ef94a3
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-09-02 10:23:33 +0900

    Redirect screen update to Rust implementation

---
+++ src/drawscreen_rs.h
@@ -0,0 +1,9 @@
+#ifndef DRAWSCREEN_RS_H
+#define DRAWSCREEN_RS_H
+
+#include <stdint.h>
+
+void rs_drawscreen_init(int width, int height);
+void rs_update_screen(int type_arg);
+
+#endif // DRAWSCREEN_RS_H
