1225b78fa032930eee576be3bda3035242d7be8a
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-09-02 09:48:19 +0900

    Move buffer setup to Rust

--- src/rust_buffer.h
+++ src/rust_buffer.h
@@ -2,8 +2,9 @@
 #define RUST_BUFFER_H
 
 #include <stddef.h>
+#include "structs.h"
 
 void *buf_alloc(size_t size);
-void buf_freeall(void *buf, int flags);
+void buf_freeall(buf_T *buf, int flags);
 
 #endif // RUST_BUFFER_H
