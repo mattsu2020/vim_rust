8a12b75d150bc4eb55bbd134a2bb3f85b52103fa
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-09-03 20:35:32 +0900

    Move file I/O wrappers into Rust

--- src/fileio_rs.h
+++ src/fileio_rs.h
@@ -7,9 +7,9 @@
 extern "C" {
 #endif
 
-int rs_readfile(const char *fname, const char *sfname, long from, long lines_to_skip,
-                long lines_to_read, void *eap, int flags);
-int rs_writefile(const char *fname, const char *data, size_t len, int flags);
+int readfile(const char *fname, const char *sfname, long from, long lines_to_skip,
+             long lines_to_read, void *eap, int flags);
+int writefile(const char *fname, const char *data, size_t len, int flags);
 
 #ifdef __cplusplus
 }
