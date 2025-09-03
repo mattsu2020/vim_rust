c0fb365655088f7a0afe69dc46fbdaccefdbec9c
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-09-03 20:35:25 +0900

    Port screen drawing to Rust

--- src/drawscreen_rs.h
+++ src/drawscreen_rs.h
@@ -2,8 +2,17 @@
 #define DRAWSCREEN_RS_H
 
 #include <stdint.h>
+#include "screen_rs.h"
 
-void rs_drawscreen_init(int width, int height);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void rs_drawscreen_init(ScreenBuffer *buf, int width, int height);
 void rs_update_screen(int type_arg);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // DRAWSCREEN_RS_H
