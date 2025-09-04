3a1c0fcb410ee54ca63562428c61e07eb0fc44a9
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-09-04 16:10:52 +0900

    src: remove unused FFI headers

--- src/rust_autocmd.h
+++
@@ -1,8 +0,0 @@
-#ifndef RUST_AUTOCMD_H
-#define RUST_AUTOCMD_H
-
-#include "vim.h"
-
-int rust_autocmd_do(int event, const char *name);
-
-#endif // RUST_AUTOCMD_H
--- src/rust_bufwrite.h
+++
@@ -1,8 +0,0 @@
-#ifndef RUST_BUFWRITE_H
-#define RUST_BUFWRITE_H
-
-#include <stddef.h>
-
-int bufwrite_dummy(const char *path);
-
-#endif // RUST_BUFWRITE_H
