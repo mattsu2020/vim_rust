29bbc47f92ef889969642509ef37d2e805a20a26
mattsu <35655889+mattsu2020@users.noreply.github.com>
2025-09-04 10:06:36 +0900

    Port normal command handling to Rust

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
     println!("cargo:rerun-if-changed=src/nv_cmds.h");
+    println!("cargo:rerun-if-changed=src/normal.c");
     create_nvcmdidxs::generate_file("src/nv_cmdidxs.h").expect("failed to generate nv_cmdidxs.h");
 }
